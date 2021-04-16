0c51e3e4accdf661da9d48b4b48b7d77e5e4bc95
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-04-16 11:58:43 +0530

    titile changes

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Pixo Gallery</h1>
+      <h2>Save Your Moments</h2>
+      <p> Keep Your Photos With You Everywhere...!</p>
     </div>
   )
 }
